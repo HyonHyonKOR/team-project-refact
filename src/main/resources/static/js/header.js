ca15ef0e742f2c0fc8d309c4f0318cd9d1919e2f
HyonHyon <khm3626@gmail.com>
2024-02-25 04:19:56 +0900

    refactor: complete responsive header

--- src/main/resources/static/js/header.js
+++ src/main/resources/static/js/header.js
@@ -1,5 +1,18 @@
 'use strict'
 
+/*헤더 쇼핑몰 버튼 검색*/
+const headerShopMenuLink = document.querySelector('#nav-shop__link');
+const tablet = window.matchMedia('(min-width: 769px)').matches;
+headerShopMenuLink.addEventListener('click',(event)=>{
+    if(window.matchMedia('(min-width: 769px)').matches){
+        return;
+    }
+    event.preventDefault();
+    const navShopMenus = document.querySelector('.nav-shop__menus');
+    navShopMenus.classList.toggle('active');
+})
+
+
 /*헤더 검색 버튼 토글*/
 const headerItemSearchLink = document.querySelector('.header__nav-search');
 
@@ -7,6 +20,17 @@ headerItemSearchLink.addEventListener('click',()=>{
     const headerItemSearchContainer = document.querySelector('.nav-search__menus');
     headerItemSearchContainer.classList.toggle('active');
 });
+
+
+/*헤더 반응형 토글*/
+const headerToggle = document.querySelector('.header__toggle');
+
+headerToggle.addEventListener('click',()=>{
+    const headerNav = document.querySelector('.header__nav');
+    headerNav.classList.toggle('active');
+});
+
+
 
 
 /*로그인 헤더 로그아웃*/
